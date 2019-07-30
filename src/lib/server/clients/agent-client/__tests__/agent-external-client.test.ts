899653377de4c751515e796b5cde084f0c7de760
AnnaLaznia <anna.laznia@gmail.com>
2019-07-30 20:32:39 +0000

    New test

--- src/lib/server/clients/agent-client/__tests__/agent-external-client.test.ts
+++ src/lib/server/clients/agent-client/__tests__/agent-external-client.test.ts
@@ -56,11 +56,11 @@ test('Заполненный список билетов', async () => {
   // - flight1, 2019.09.15 13:00
   // - flight2, 2019.09.15 22:00
   await client.schedule.insert({
-    departureDate: new Date(2019, 8, 15, 13).toUTCString(),
+    departureDate: new Date(2019, 9, 15, 13).toUTCString(),
     flightId: flight1.id,
   });
   await client.schedule.insert({
-    departureDate: new Date(2019, 8, 15, 22).toUTCString(),
+    departureDate: new Date(2019, 9, 15, 22).toUTCString(),
     flightId: flight2.id,
   });
   // 56
@@ -97,7 +97,7 @@ test('Заполненный список билетов', async () => {
   const ticketList = await client.searchTickets({
     departureCityId: dubai.id,
     destinationCityId: berlin.id,
-    departureDate: new Date(2019, 8, 15).toUTCString(),
+    departureDate: new Date(2019, 9, 15).toUTCString(),
   });
 
   // ### Ожидаемый результат
@@ -117,9 +117,9 @@ test('Заполненный список билетов', async () => {
       price: 400,
       oldPrice: 400,
       departureAirport: dubaiAirport,
-      departureDate: new Date(2019, 8, 15, 22).toUTCString(),
+      departureDate: new Date(2019, 9, 15, 22).toUTCString(),
       destinationAirport: berlinAirport,
-      destinationDate: new Date(2019, 8, 16, 1).toUTCString(),
+      destinationDate: new Date(2019, 9, 16, 1).toUTCString(),
       type: ETicketType.Sweet,
       class: economyClass,
       fareBase: NRL08LS,
@@ -141,9 +141,9 @@ test('Заполненный список билетов', async () => {
       price: 440,
       oldPrice: 440,
       departureAirport: dubaiAirport,
-      departureDate: new Date(2019, 8, 15, 22).toUTCString(),
+      departureDate: new Date(2019, 9, 15, 22).toUTCString(),
       destinationAirport: berlinAirport,
-      destinationDate: new Date(2019, 8, 16, 1).toUTCString(),
+      destinationDate: new Date(2019, 9, 16, 1).toUTCString(),
       type: ETicketType.GDS,
       class: economyClass,
       fareBase: NRL08LS,
@@ -165,9 +165,9 @@ test('Заполненный список билетов', async () => {
       price: 500,
       oldPrice: 500,
       departureAirport: dubaiAirport,
-      departureDate: new Date(2019, 8, 15, 13).toUTCString(),
+      departureDate: new Date(2019, 9, 15, 13).toUTCString(),
       destinationAirport: berlinAirport,
-      destinationDate: new Date(2019, 8, 15, 16).toUTCString(),
+      destinationDate: new Date(2019, 9, 15, 16).toUTCString(),
       type: ETicketType.Sweet,
       class: economyClass,
       fareBase: NRL09KS,
@@ -189,9 +189,9 @@ test('Заполненный список билетов', async () => {
       price: 550,
       oldPrice: 550,
       departureAirport: dubaiAirport,
-      departureDate: new Date(2019, 8, 15, 13).toUTCString(),
+      departureDate: new Date(2019, 9, 15, 13).toUTCString(),
       destinationAirport: berlinAirport,
-      destinationDate: new Date(2019, 8, 15, 16).toUTCString(),
+      destinationDate: new Date(2019, 9, 15, 16).toUTCString(),
       type: ETicketType.GDS,
       class: economyClass,
       fareBase: NRL09KS,
@@ -201,5 +201,107 @@ test('Заполненный список билетов', async () => {
   ];
   expect(ticketList).toHaveLength(4);
   expect(ticketList).toEqual(expected);
-  123
+});
+
+test('0 билетов если поиск на неверную дату', async () => {
+  // const t = async () => {
+  // ### Изначальные условия
+
+  // В таблицу cities добавляем города:
+  // - Дубай (dub)
+  // - Берлин (ber)
+
+  const dubai = await client.cities.insert({
+    name: 'Дубай',
+    abbreviation: 'dub',
+  });
+  const berlin = await client.cities.insert({
+    name: 'Берлин',
+    abbreviation: 'ber',
+  });
+
+  // В таблицу airports добавляем аэропорты:
+  // - Дубай, cityId:  dub (DXB)
+  // - Берлин, cityId:  ber (BER)
+  const dubaiAirport = await client.airports.insert({
+    name: 'Дубай',
+    cityId: dubai.id,
+  });
+  const berlinAirport = await client.airports.insert({
+    name: 'Берлин',
+    cityId: berlin.id,
+  });
+
+  // В таблицу airlines добавляем перевозчика:
+  // - Delta (delta)
+  const deltaAirlenes = await client.airlines.insert({ name: 'Delta' });
+
+  // В таблицу flights добавляем рейсы:
+  // - DXB → BER, 3 часа, delta, (flight1)
+  // - DXB → BER, 3 часа, delta, (flight2)
+  const flight1 = await client.flights.insert({
+    departureAirportId: dubaiAirport.id,
+    destinationAirportId: berlinAirport.id,
+    duration: 180,
+    airlineId: deltaAirlenes.id,
+  });
+  const flight2 = await client.flights.insert({
+    departureAirportId: dubaiAirport.id,
+    destinationAirportId: berlinAirport.id,
+    duration: 180,
+    airlineId: deltaAirlenes.id,
+  });
+
+  // В таблицу schedule добавляем строки:
+  // - flight1, 2019.09.15 13:00
+  // - flight2, 2019.09.15 22:00
+  await client.schedule.insert({
+    departureDate: new Date(2019, 9, 15, 13).toUTCString(),
+    flightId: flight1.id,
+  });
+  await client.schedule.insert({
+    departureDate: new Date(2019, 9, 15, 22).toUTCString(),
+    flightId: flight2.id,
+  });
+  // 56
+
+  // В таблицу classes добавляем строки:
+  // - Economy (economy)
+  const economyClass = await client.classes.insert({ name: 'Economy' });
+
+  // В таблицу fare_basises добавляем строки:
+
+  // economy, isPublic: true, price: 500,
+  // amount: 30, flightId: flight1 (NRL09KS)
+
+  // economy, isPublic: true, price: 400,
+  // amount: 30, flightId: flight2 (NRL08LS)
+  const NRL09KS = await client.fareBases.insert({
+    classId: economyClass.id,
+    isPublic: true,
+    price: 500,
+    amount: 30,
+    flightId: flight1.id,
+  });
+  const NRL08LS = await client.fareBases.insert({
+    classId: economyClass.id,
+    isPublic: true,
+    price: 400,
+    amount: 30,
+    flightId: flight2.id,
+  });
+
+  // ### Действие
+
+  // Выполняем поиск билетов из Дубай (DXB) — Берлин (BER) на 2019.09.15
+  const ticketList = await client.searchTickets({
+    departureCityId: dubai.id,
+    destinationCityId: berlin.id,
+    departureDate: new Date(2019, 9, 16).toUTCString(),
+  });
+
+  // ### Ожидаемый результат
+
+  expect(ticketList).toHaveLength(0);
+
 });
